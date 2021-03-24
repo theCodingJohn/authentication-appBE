4d8958357e9a52c067cfe960405df906732dfda4
John Carlo Cunanan <codingjohn@outlook.com>
2021-03-24 17:22:42 +0800

    Add login router

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import middleware from "./utils/middleware.js";
 
 // Router
 import usersRouter from "./controllers/user.controller.js";
+import loginRouter from "./controllers/login.controller.js";
 
 const app = express();
 
@@ -33,6 +34,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/users", usersRouter);
+app.use("/users", loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
---
+++ controllers/login.controller.js
@@ -0,0 +1,37 @@
+import express from "express";
+import bcryt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+import catchAsync from "../utils/catchAsync.js";
+
+const loginRouter = express.Router();
+
+loginRouter.post(
+  "/login",
+  catchAsync(async (req, res, next) => {
+    const body = req.body;
+
+    const user = await User.findOne({ email: body.email });
+    const passwordCorrect =
+      user !== null
+        ? await bcryt.compare(body.password, user.passwordHash)
+        : null;
+
+    if (!(user && passwordCorrect)) {
+      return response.status(401).json({
+        error: "invalid username or password",
+      });
+    }
+
+    const userForToken = {
+      email: user.email,
+      id: user._id,
+    };
+
+    const token = jwt.sign(userForToken, process.env.SECRET);
+
+    res.status(200).send({ token, email: user.email, name: user?.name });
+  })
+);
+
+export default loginRouter;
