b79e9d4a587b0ccd1143bf0189010c9928ba3fd1
John Carlo Cunanan <codingjohn@outlook.com>
2021-03-26 22:25:26 +0800

    Add error message on JsonWebTokenError

--- utils/middleware.js
+++ utils/middleware.js
@@ -36,7 +36,7 @@ const errorHandler = (error, req, res, next) => {
   if (error.name === "ValidationError") {
     return res.status(400).json({ error: error.message });
   } else if (error.name === "JsonWebTokenError") {
-    return res.status(401).json({ error: error.message });
+    return res.status(401).json({ error: "invalid token" });
   }
   next(error);
 };
